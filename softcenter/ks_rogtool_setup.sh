37ae3f1ccf43869dff10e77af1f4dd60be190cb5
Kevin Wee <kevinwee@163.com>
2021-12-29 11:09:46 +0800

    Delete ks_rogtool_setup.sh

--- softcenter/ks_rogtool_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /tmp
-wget --no-check-certificate https://rogsoft.ddnsto.com/softcenter/ks_rogtool_setup
-chmod +x ./ks_rogtool_setup
-./ks_rogtool_setup install
-rm -rf /tmp/ks_rogtool_setup
