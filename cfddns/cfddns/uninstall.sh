027ae3c8c3fcb350b1effa7d268a02a8841a9b00
Kevin Wee <kevinwee@163.com>
2021-12-29 09:59:34 +0800

    Delete cfddns directory

--- cfddns/cfddns/uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-source /koolshare/scripts/base.sh
-
-find /koolshare/init.d/ -name "*cfddns*" | xargs rm -rf
-rm -rf /koolshare/res/icon-cfddns.png
-rm -rf /koolshare/scripts/cfddns*.sh
-rm -rf /koolshare/scripts/uninstall_cfddns.sh
-rm -rf /koolshare/webs/Module_cfddns.asp
