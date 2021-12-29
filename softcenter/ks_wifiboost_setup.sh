fa4a7ea3f58165c3c11c229870c204278227ce12
Kevin Wee <kevinwee@163.com>
2021-12-29 11:10:18 +0800

    Delete ks_wifiboost_setup.sh

--- softcenter/ks_wifiboost_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd /tmp
-rm -rf /tmp/wifiboost*
-rm -rf /tmp/ks_wifiboost_setup
-wget --no-check-certificate https://rogsoft.ddnsto.com/softcenter/ks_wifiboost_setup
-chmod +x ./ks_wifiboost_setup
-./ks_wifiboost_setup install
-rm -rf /tmp/ks_wifiboost_setup
